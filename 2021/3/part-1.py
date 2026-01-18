e45dac56efc2d326dd3883e7762a3c32cf5d932c
Raspreet Singh <raspreet800@gmail.com>
2026-01-18 09:46:11 +0530

    add solutions from AOC 2021

---
+++ 2021/3/part-1.py
@@ -0,0 +1,15 @@
+from parse_input import parse_input
+
+with open("input.txt") as file:
+    numbers = parse_input(file)
+
+lists = list(zip(*map(list, numbers)))
+
+from collections import Counter
+
+most_common_bit = int("".join([i.most_common(1)[0][0] for i in map(Counter, lists)]), 2)
+least_common_bit = int(
+    "".join([i.most_common()[-1][0] for i in map(Counter, lists)]), 2
+)
+
+print(most_common_bit * least_common_bit)
